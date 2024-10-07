3b0327064a90b3a745a75cf916e16424548fac7a
ISankrityayan <ishitasankrityayan@gmail.com>
2024-10-07 19:48:22 +0530

    Time: 0 ms (100%), Space: 41.2 MB (82.08%) - LeetHub

--- 0961-long-pressed-name/0961-long-pressed-name.java
+++ 0961-long-pressed-name/0961-long-pressed-name.java
@@ -1,35 +1,38 @@
 class Solution {
     public boolean isLongPressedName(String name, String typed) {
-        int len1=name.length();
-        int len2=typed.length();
-        int idx1=0;
-        int idx2=0;
-        char prev=name.charAt(0);
-        char c2=typed.charAt(0);
-        char c1=prev;
-        while(idx1<len1 && idx2<len2){
-            c1=name.charAt(idx1);
-            c2=typed.charAt(idx2);
-            if(c1==c2){
+        int n1 = name.length();
+        int n2 = typed.length();
+        int idx1 = 0, idx2 = 0;
+
+        while (idx1 < n1 && idx2 < n2) {
+            // If characters match, increment both pointers
+            if (name.charAt(idx1) == typed.charAt(idx2)) {
                 idx1++;
                 idx2++;
-                prev=c1;
-            }
-            else if(c2==prev){
-                idx2++;
-
-            }
-            else{
+            } 
+            // If characters don't match, but typed has the previous character long-pressed
+            else if (idx1 > 0 && name.charAt(idx1 - 1) == typed.charAt(idx2)) {
+                idx2++; // Only increment idx2 for long press
+            } 
+            // If neither of the above conditions is met, it's not a valid long press
+            else {
                 return false;
             }
         }
-        while(idx2<len2){
-            c2=typed.charAt(idx2);
-            if(c2!=c1){
+
+        // After looping, ensure all characters in `name` are checked
+        if (idx1 < n1) {
+            return false;
+        }
+
+        // Ensure the remaining characters in `typed` are just long presses of the last character in `name`
+        while (idx2 < n2) {
+            if (name.charAt(n1 - 1) != typed.charAt(idx2)) {
                 return false;
             }
             idx2++;
         }
-        return idx1==len1 && idx2==len2;
+
+        return true;
     }
 }
