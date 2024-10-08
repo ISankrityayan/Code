b118d4be51d9cfe71b78063a989d1db35a1ec0d0
ISankrityayan <ishitasankrityayan@gmail.com>
2024-10-08 20:13:49 +0530

    Time: 0 ms (100%), Space: 41.6 MB (75.32%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.java
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.java
@@ -1,16 +1,23 @@
 class Solution {
     public String mergeAlternately(String word1, String word2) {
-        StringBuilder res=new StringBuilder();
-        int i=0;
-        while(i<word1.length() || i<word2.length()){
-            if(i<word1.length()){
-                res.append(word1.charAt(i));
-            }
-            if(i<word2.length()){
-                res.append(word2.charAt(i));
-            }
+        int n = word1.length(), m = word2.length();
+        StringBuilder res = new StringBuilder(n + m); // Pre-allocate expected size
+        
+        int i = 0;
+        // Merge the common length portion
+        while (i < n && i < m) {
+            res.append(word1.charAt(i));
+            res.append(word2.charAt(i));
             i++;
         }
+        
+        // Append the remaining part of the longer string
+        if (i < n) {
+            res.append(word1.substring(i));
+        } else if (i < m) {
+            res.append(word2.substring(i));
+        }
+        
         return res.toString();
     }
 }
