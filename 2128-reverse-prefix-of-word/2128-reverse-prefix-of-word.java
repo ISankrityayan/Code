de673f58ff3974c150981a76b47e6d6aa7e2c656
ISankrityayan <ishitasankrityayan@gmail.com>
2024-10-08 20:29:27 +0530

    Time: 1 ms (54.91%), Space: 41.4 MB (79.6%) - LeetHub

--- 2128-reverse-prefix-of-word/2128-reverse-prefix-of-word.java
+++ 2128-reverse-prefix-of-word/2128-reverse-prefix-of-word.java
@@ -1,17 +1,9 @@
 class Solution {
     public String reversePrefix(String word, char ch) {
-        int prefixIndex = word.indexOf(ch);
-        
-        // If ch is not found, return the original word
-        if (prefixIndex == -1) {
-            return word;
+        int j = word.indexOf(ch);
+        if (j != -1) {
+            return new StringBuilder(word.substring(0, j + 1)).reverse().toString() + word.substring(j + 1);
         }
-        
-        // Reverse the prefix using StringBuilder
-        StringBuilder reversedPrefix = new StringBuilder(word.substring(0, prefixIndex + 1));
-        reversedPrefix.reverse();
-        
-        // Combine reversed prefix with the rest of the string
-        return reversedPrefix.toString() + word.substring(prefixIndex + 1);
+        return word;
     }
 }
