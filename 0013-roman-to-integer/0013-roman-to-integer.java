ef61e5d2bcfc3711929f9aa8c0a3003f340e5d18
ISankrityayan <ishitasankrityayan@gmail.com>
2024-10-09 01:19:06 +0530

    Time: 4 ms (69.15%), Space: 44.5 MB (83.67%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,23 +1,29 @@
 class Solution {
     public int romanToInt(String s) {
-        int res=0;
-        Map<Character,Integer>roman=new HashMap<>();
-        roman.put('I',1);
-        roman.put('V',5);
-        roman.put('X',10);
-        roman.put('L',50);
-        roman.put('C',100);
-        roman.put('D',500);
-        roman.put('M',1000);
+        // Create an array for storing Roman numeral values
+        int[] roman = new int[91]; // ASCII value of 'M' is 77, so we take 91 for safety
+        roman['I'] = 1;
+        roman['V'] = 5;
+        roman['X'] = 10;
+        roman['L'] = 50;
+        roman['C'] = 100;
+        roman['D'] = 500;
+        roman['M'] = 1000;
 
-        for(int i=0;i<s.length()-1;i++){
-            if(roman.get(s.charAt(i))<roman.get(s.charAt(i+1))){
-                res-=roman.get(s.charAt(i));
-            }
-            else{
-                res+=roman.get(s.charAt(i));
+        int res = 0;
+
+        // Traverse through the Roman numeral string
+        for (int i = 0; i < s.length(); i++) {
+            // If current value is less than the next value, subtract it (for cases like IV = 4)
+            if (i < s.length() - 1 && roman[s.charAt(i)] < roman[s.charAt(i + 1)]) {
+                res -= roman[s.charAt(i)];
+            } 
+            // Otherwise, just add the value
+            else {
+                res += roman[s.charAt(i)];
             }
         }
-        return res+roman.get(s.charAt(s.length()-1));
+
+        return res;
     }
 }
