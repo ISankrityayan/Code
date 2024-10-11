f82878d5e3aeb998522b433ebed48742d61e5486
ISankrityayan <ishitasankrityayan@gmail.com>
2024-10-11 15:27:25 +0530

    Time: 0 ms (100%), Space: 40.6 MB (14.86%) - LeetHub

--- 0556-next-greater-element-iii/0556-next-greater-element-iii.java
+++ 0556-next-greater-element-iii/0556-next-greater-element-iii.java
@@ -1,32 +1,37 @@
 class Solution {
     public int nextGreaterElement(int n) {
-        long m=n;
-        int[] count=new int[10];
-        int currRem=-1;
-        int prevRem=-1;
-        while(m>0){
-            currRem=(int)m%10;
-            m=m/10;
-            count[currRem]++;
-            if(prevRem>currRem){
-                int num=currRem+1;
-                while(count[num]==0){
+        long m = n;
+        int[] count = new int[10];
+        int currRem = -1;
+        int prevRem = -1;
+        
+        while (m > 0) {
+            currRem = (int) m % 10; // Get the last digit of m
+            m = m / 10;              // Remove the last digit from m
+            count[currRem]++;        // Count the occurrence of this digit
+            
+            if (prevRem > currRem) { // Check if previous digit is greater than current
+                int num = currRem + 1; // Find the next higher digit
+                while (count[num] == 0) {
                     num++;
                 }
-                count[num]--;
-                m=m*10+num;
-
-                for(int i=0;i<10;i++){
-                    while(count[i]-->0){
-                        m=m*10+i;
-
+                count[num]--;          // Use this digit
+                m = m * 10 + num;      // Append it back to m
+                
+                // Append the smallest possible digits to form the smallest number
+                for (int i = 0; i < 10; i++) {
+                    while (count[i]-- > 0) {
+                        m = m * 10 + i;
                     }
                 }
-                return m> Integer.MAX_VALUE ? -1 : (int)m;
+                
+                // Check if the result fits in a 32-bit integer
+                return m > Integer.MAX_VALUE ? -1 : (int) m;
             }
-            prevRem=currRem;
+            
+            prevRem = currRem; // Update prevRem for the next iteration
         }
-
-        return -1;
+        
+        return -1; // If no greater number is found
     }
 }
