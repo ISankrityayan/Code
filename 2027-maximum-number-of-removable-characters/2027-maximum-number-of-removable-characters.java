09df6c9c2c6d5ff915f99d0cc155fa0f44e2ebef
ISankrityayan <ishitasankrityayan@gmail.com>
2024-10-12 21:15:14 +0530

    Time: 97 ms (52.33%), Space: 55.9 MB (90.41%) - LeetHub

--- 2027-maximum-number-of-removable-characters/2027-maximum-number-of-removable-characters.java
+++ 2027-maximum-number-of-removable-characters/2027-maximum-number-of-removable-characters.java
@@ -1,28 +1,44 @@
 class Solution {
-    public int maximumRemovals(String s, String p, int[] removable) {
-        int low = 1, high = removable.length, pivot;
-        while (low <= high) {
-            pivot = low + (high - low) / 2;
-            if (isASubsequenceAfterRemoving(s.toCharArray(), p.toCharArray(), removable, pivot)) {
-                low = pivot + 1;
-            } else {
-                high = pivot - 1;
-            }
-        }
-        return low - 1;
+   public int maximumRemovals(String s, String p, int[] rem) {
+    int left = 0, right = rem.length;
+    int[] removalOrder = new int[s.length()];
+    
+    // Fill the removalOrder array with rem.length initially, meaning no removals
+    Arrays.fill(removalOrder, rem.length);
+    
+    // Populate the removalOrder array where each rem[i] stores the removal index
+    for (int i = 0; i < rem.length; i++) {
+        removalOrder[rem[i]] = i;
     }
+    
+    // Binary search to find the maximum `m` removals
+    int maxM = 0;
+    while (left < right) {
+        int mid = (left + right + 1) / 2;
+        if (isSubsequenceAfterRemovals(s, p, removalOrder, mid)) {
+            left = mid;  // Move towards higher m if valid
+            maxM = mid;  // Update the maxM value
+        } else {
+            right = mid - 1;  // Try smaller m values
+        }
+    }
+    
+    return maxM;  // Return the maxM (maximum `m` removals)
+}
 
-    private boolean isASubsequenceAfterRemoving(char[] s, char[] p, int[] removable, int limit) {
-        for (int i = 0; i < limit; i++) {
-            s[removable[i]] = 0;
+// Helper method to check if `p` is still a subsequence of `s` after `maxRemovals`
+private boolean isSubsequenceAfterRemovals(String s, String p, int[] removalOrder, int maxRemovals) {
+    int j = 0;  // Pointer for `p`
+    
+    // Traverse `s` and check if characters of `p` can still be matched
+    for (int i = 0; i < s.length() && j < p.length(); i++) {
+        // Check if the character at `i` is not removed or was removed after `maxRemovals`
+        if (removalOrder[i] >= maxRemovals && s.charAt(i) == p.charAt(j)) {
+            j++;  // Move to the next character in `p`
         }
-        int i = 0, j = 0;
-        while (i < s.length && j < p.length) {
-            if (s[i] == p[j]) {
-                j++;
-            }
-            i++;
-        }
-        return j == p.length;
     }
+    
+    return j == p.length();  // True if all characters of `p` are matched
+}
+
 }
