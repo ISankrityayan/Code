5fce18a49a04a503b1908fefe3d5217a99a29245
ISankrityayan <ishitasankrityayan@gmail.com>
2024-11-10 19:53:48 +0530

    Time: 7 ms (17.21%), Space: 46.7 MB (36.05%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] countBits(int n) {
+        int arr[]=new int[n+1];
+        for(int i=0;i<=n;i++){
+            int bits=0,mask=1;
+            for(int j=0;j<32;j++){
+                if((mask&i)!=0){
+                bits++;
+            }
+            mask<<=1;
+            } 
+            arr[i]=bits;
+        }
+        return arr;
+    }
+}
