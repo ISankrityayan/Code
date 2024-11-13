ef31e6cc869bf8480a2e9f0683b5c9f90d4b3723
ISankrityayan <ishitasankrityayan@gmail.com>
2024-11-14 01:06:43 +0530

    Time: 4 ms (86.6%), Space: 40.3 MB (71.12%) - LeetHub

--- 0767-prime-number-of-set-bits-in-binary-representation/0767-prime-number-of-set-bits-in-binary-representation.java
+++ 0767-prime-number-of-set-bits-in-binary-representation/0767-prime-number-of-set-bits-in-binary-representation.java
@@ -1,25 +1,24 @@
 class Solution {
     public int countPrimeSetBits(int left, int right) {
-        int ans = 0;
-        // Bitmask representing prime numbers up to 32 (bit positions for 2, 3, 5, 7, 11, etc., are set to 1)
-        int primeMask = 0b10100010100010101100; // This binary value has bits set at prime indices (2, 3, 5, 7, etc.)
-
-        for (int i = left; i <= right; i++) {
-            int count = countSetBits(i);
-            if ((primeMask & (1 << count)) != 0) { // Check if count is a prime by testing the corresponding bit in primeMask
-                ans++;
+        int primecount=0;
+        for(int i=left;i<=right;i++){
+            int setBitCount=Integer.bitCount(i);
+            if(isPrime(setBitCount)){
+                primecount++;
             }
         }
-        return ans;
+               
+        return  primecount++;
+    }
+    public boolean isPrime(int num){
+       if(num<=1)return false;
+        for(int i=2; i*i<=num; i++){
+            if(num % i == 0){
+                return false;
+            }
+        }
+        return true;
     }
 
-    // Function to count set bits using bit manipulation
-    private int countSetBits(int n) {
-        int count = 0;
-        while (n != 0) {
-            count += (n & 1); // Check if the last bit is set
-            n >>= 1; // Shift right to move to the next bit
-        }
-        return count;
-    }
+
 }
