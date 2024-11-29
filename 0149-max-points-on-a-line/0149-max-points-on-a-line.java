b5160337dacaee71140ce068458b49ae18e36d17
ISankrityayan <ishitasankrityayan@gmail.com>
2024-11-29 23:58:15 +0530

    Time: 9 ms (98.77%), Space: 41.6 MB (83.42%) - LeetHub

--- 0149-max-points-on-a-line/0149-max-points-on-a-line.java
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.java
@@ -1,26 +1,28 @@
 class Solution {
     public int maxPoints(int[][] points) {
-        int maxPoints = 0;
-    for (int i = 0; i < points.length; i++) {
-      int[] point1 = points[i];
-      for (int j = i + 1; j < points.length; j++) {
-        int[] point2 = points[j];
-        double slope = (double) (point2[1] - point1[1]) / (point2[0] - point1[0]);
-        int numPointsOnLine = 2;  // point1 and point2 are already on the line
-        for (int k = 0; k < points.length; k++) {
-          if (k == i || k == j) {
-            continue;  // skip point1 and point2
-          }
-          int[] point3 = points[k];
-          if ((double) (point3[1] - point1[1]) / (point3[0] - point1[0]) == slope) {
-            numPointsOnLine++;
-          }
+        int m=points.length;
+        if (m<2)
+        {
+            return m;
         }
-        maxPoints = Math.max(maxPoints, numPointsOnLine);
-      }
-    }
-    if(maxPoints==0)
-    return 1;
-    return maxPoints;
+        int max=0;
+        for(int i=0;i<m;i++)
+        {
+            for(int j=i+1;j<m;j++)
+            {
+                int x1=points[i][0],y1=points[i][1],x2=points[j][0],y2=points[j][1];
+                int count=2;
+                for(int k=j+1;k<m;k++)
+                {
+                    int x3=points[k][0],y3=points[k][1];
+                    if ((x2-x1)*(y3-y1)==(x3-x1)*(y2-y1))
+                    {
+                        count ++;
+                    }
+                }
+                max=Math.max(count,max);
+            }
+        }
+        return max;
     }
 }
