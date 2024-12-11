a29c5f6ab561eb7838646c986227fb8463c87fbc
ISankrityayan <ishitasankrityayan@gmail.com>
2024-12-11 17:46:04 +0530

    Time: 5 ms (27.14%), Space: 42.7 MB (7.8%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,28 +1,32 @@
+
+
 class Solution {
     public List<String> letterCombinations(String digits) {
-
-        if (digits == null || digits.length() == 0) {
-            return new ArrayList<>();
+        if (digits == null || digits.isEmpty()) {
+            return new ArrayList<>(); // Return an empty list if input is null or empty
         }
-        Map<Character, String> map = Map.of(
-            '2', "abc", '3', "def", '4', "ghi", '5', "jkl", '6', "mno", '7', "pqrs", '8', "tuv", '9', "wxyz"
-        ); 
 
-        List<String> o = new ArrayList<>();
-        helper(map, o, digits, "");
-        return o;
+        Map<Character, String> charArray = Map.of(
+                '2', "abc", '3', "def", '4', "ghi", '5', "jkl",
+                '6', "mno", '7', "pqrs", '8', "tuv", '9', "wxyz"
+        );
+
+        List<String> res = new ArrayList<>();
+        getLetterCombinations("", digits, charArray, res);
+        return res;
     }
 
-    public void helper(Map<Character, String> map, List<String> o, String digits, String p) {
-        if (digits.length() == 0) {
-            o.add(p);
+    private void getLetterCombinations(String p, String up, Map<Character, String> charArray, List<String> res) {
+        if (up.isEmpty()) {
+            res.add(p);
             return;
         }
-        char d = digits.charAt(0);
-        String words = map.get(d);
-        digits = digits.substring(1);
-        for (int i=0; i<words.length(); i++) {
-            helper(map, o, digits, p + words.charAt(i));
+        char digit = up.charAt(0);
+        String letters = charArray.get(digit);
+        if (letters == null) return;
+
+        for (int i = 0; i < letters.length(); i++) {
+            getLetterCombinations(p + letters.charAt(i), up.substring(1), charArray, res);
         }
-    } 
+    }
 }
