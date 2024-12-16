7eb0fd76f9fa81255b0249f3c283daa71f4519a9
ISankrityayan <ishitasankrityayan@gmail.com>
2024-12-16 23:57:54 +0530

    Time: 229 ms (20.22%), Space: 41.3 MB (24.27%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.java
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -4,34 +4,75 @@ class Solution {
     }
 
     private boolean solve(char[][] board) {
-        for (int row = 0; row < 9; row++) {
-            for (int col = 0; col < 9; col++) {
-                if (board[row][col] == '.') {
-                    for (char num = '1'; num <= '9'; num++) {
-                        if (isValid(board, row, col, num)) {
-                            board[row][col] = num;
-                            if (solve(board)) return true;
-                            board[row][col] = '.';
-                        }
-                    }
+        int n = board.length;
+        int row = -1;
+        int col = -1;
+        boolean emptyLeft = true;
+
+        // Find an empty cell ('.') in the Sudoku board
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == '.') {
+                    row = i;
+                    col = j;
+                    emptyLeft = false;
+                    break;
+                }
+            }
+            if (!emptyLeft) {
+                break;
+            }
+        }
+
+        // Base case: If no empty cell is found, the Sudoku is solved
+        if (emptyLeft) {
+            return true;
+        }
+
+        // Try placing digits 1-9 in the empty cell
+        for (int number = 1; number <= 9; number++) {
+            if (isSafe(board, row, col, number)) {
+                board[row][col] = (char) (number + '0'); // Convert int to char
+                if (solve(board)) {
+                    return true; // If placing this number solves the puzzle
+                } else {
+                    board[row][col] = '.'; // Backtrack
+                }
+            }
+        }
+
+        return false; // No valid number found for this position
+    }
+
+    private boolean isSafe(char[][] board, int row, int col, int num) {
+        char charNum = (char) (num + '0'); // Convert int to char for comparison
+        int boxSize = 3;
+
+        // Check the row
+        for (int i = 0; i < board.length; i++) {
+            if (board[row][i] == charNum) {
+                return false;
+            }
+        }
+
+        // Check the column
+        for (int i = 0; i < board.length; i++) {
+            if (board[i][col] == charNum) {
+                return false;
+            }
+        }
+
+        // Check the 3x3 subgrid
+        int rowStart = row - row % boxSize;
+        int colStart = col - col % boxSize;
+        for (int i = rowStart; i < rowStart + boxSize; i++) {
+            for (int j = colStart; j < colStart + boxSize; j++) {
+                if (board[i][j] == charNum) {
                     return false;
                 }
             }
         }
-        return true;
-    }
 
-    private boolean isValid(char[][] board, int row, int col, char num) {
-        for (int i = 0; i < 9; i++) {
-            if (board[row][i] == num || board[i][col] == num) return false;
-        }
-        int startRow = (row / 3) * 3;
-        int startCol = (col / 3) * 3;
-        for (int i = startRow; i < startRow + 3; i++) {
-            for (int j = startCol; j < startCol + 3; j++) {
-                if (board[i][j] == num) return false;
-            }
-        }
-        return true;
+        return true; // Safe to place the number
     }
 }
