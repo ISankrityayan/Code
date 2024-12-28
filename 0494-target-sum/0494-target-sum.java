53cdf3fe36a1d02c244a4952504c47764778c1e0
ISankrityayan <ishitasankrityayan@gmail.com>
2024-12-28 21:58:11 +0530

    Time: 633 ms (28.48%), Space: 41.7 MB (56.46%) - LeetHub

---
+++ 0494-target-sum/0494-target-sum.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int findTargetSumWays(int[] nums, int target) {
+        return count(nums,target,0,0);
+    }
+
+    private int count(int[] nums, int target,int index,int sum){
+        if(index>=nums.length){
+            return sum==target?1:0;
+        }
+
+        int plusCount=count(nums,target,index+1,sum+nums[index]);
+        int minusCount=count(nums,target,index+1,sum-nums[index]);
+        return plusCount+minusCount;
+
+    }
+}
