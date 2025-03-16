c9d8bc6ae9ee9eaf6ea379ca80cc01dd3b1780c8
ISankrityayan <ishitasankrityayan@gmail.com>
2025-03-17 01:49:28 +0530

    Time: 27 ms (23.9%), Space: 45.4 MB (34.57%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.java
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.java
@@ -4,13 +4,13 @@ class Solution {
         int low=1;
         int high=Arrays.stream(piles).max().getAsInt();
         while(low<=high){
-            int mid=(low+high)/2;
+            int mid=low+(high-low)/2;
             int hours_taken=totalHours(piles,mid);
             if(hours_taken<=h){
-               return high=mid-1;
+                high=mid-1;
             }
             else{
-                return low=mid+1;
+                 low=mid+1;
             }
         }
                     return low;
