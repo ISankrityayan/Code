6d602cc4c44dab38ec6ed474806136a253901cb7
ISankrityayan <ishitasankrityayan@gmail.com>
2025-03-17 01:48:00 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.java
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.java
@@ -1,37 +1,32 @@
 class Solution {
-
-    public int findMax(int[]v){
-        int maxi=Integer.MIN_VALUE;
-        int n=v.length;
-        for(int i=0;i<n;i++){
-           maxi=Math.max(maxi,v[i]);
-        }
-        return maxi;
-    }
-    public int calculateTotalHours(int[]v,int hourly){
-        int totalH=0;
-        int n=v.length;
-        for(int i=0;i<n;i++){
-            totalH+=Math.ceil((double)v[i]/(double)hourly);
-        }
-        return totalH;
-    }
     public int minEatingSpeed(int[] piles, int h) {
-         int low=1;
-         int high=findMax(piles);
-         while(low<=high){
-            int mid=low+(high-low)/2;
-            int totalH=calculateTotalHours(piles,mid);
-            if(totalH<=h){
-                high=mid-1;
-
+        
+        int low=1;
+        int high=Arrays.stream(piles).max().getAsInt();
+        while(low<=high){
+            int mid=(low+high)/2;
+            int hours_taken=totalHours(piles,mid);
+            if(hours_taken<=h){
+               return high=mid-1;
             }
             else{
-                low=mid+1;
+                return low=mid+1;
             }
-         }
-         return low;
+        }
+                    return low;
+
+
     }
+    private int totalHours(int[] piles, int hourly){
+        int total_hours=0;
+        int n=piles.length;
+        for(int i=0;i<n;i++){
+           total_hours+=Math.ceil((double)piles[i]/hourly); 
+        }
+        return total_hours;
+    }
+
+
 
 
 }
